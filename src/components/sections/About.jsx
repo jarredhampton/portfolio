f7ab434e31697453e7d013d47a1720d68fce326e
Goose_tG <jarredhampton@hotmail.com>
2025-05-20 22:03:45 -0400

    update: about me inside box

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -92,26 +92,9 @@ export const About = () => {
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Cloud & DevOps</h3>
-              <div className="flex flex-wrap gap-2">
-                {cloudDevOpsSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow[0_2px_8px_rgba(59,130,246,0.2)] transition-all"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-            </div>
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Security</h3>
+                <h3 className="text-xl font-bold mb-4"> Cloud & DevOps</h3>
                 <div className="flex flex-wrap gap-2">
                   {cloudDevOpsSkills.map((tech, key) => (
                     <span
@@ -121,6 +104,22 @@ export const About = () => {
                       {tech}
                     </span>
                   ))}
+                </div>
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                  <h3 className="text-xl font-bold mb-4"> Security</h3>
+                  <div className="flex flex-wrap gap-2">
+                    {cloudDevOpsSkills.map((tech, key) => (
+                      <span
+                        key={key}
+                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow[0_2px_8px_rgba(59,130,246,0.2)] transition-all"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
 
